aba0579a10e5506e2136132d437966c091d61678
HunterWinger <WingerHunter01@gmail.com>
2025-05-04 20:20:12 -0700

    Feat: Added the three GET functionalities.

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 import requests
 
+base_url = "http://chrisbrooks.pythonanywhere.com/api/programmers"
 
 def get_programmer_count():
     """
-    Return the number of programmers return from the plural programmers API
+    Return the number of programmers returned from the plural programmers API
     :return: An integer indicating the number of programmers in the plural list.
     """
+    req = requests.get(base_url)
+    if req.status_code == 200:
+        data = req.json()
+        return len(data.get("programmers", []))
     return 0
 
 
@@ -15,6 +20,9 @@ def get_programmer_by_id(pid):
     :param pid: Unique identifier for the programmer to lookup
     :return: A dictionary containing the matched programmer. Return an empty dictionary if not found
     """
+    req = requests.get(f"{base_url}/{pid}")
+    if req.status_code == 200:
+        return req.json()
     return {}
 
 
@@ -24,4 +32,11 @@ def get_full_name_from_first(first_name):
     :param first_name:
     :return: A string containing the first and last name of the first programmer in the list of matches.
     """
-    return ""
+    r = requests.get(f"{base_url}/by_first_name/{first_name}")
+    if r.status_code == 200:
+        data = r.json()
+        programmers = data.get("programmers", [])
+        if programmers:
+            prog = programmers[0]
+            return f"{prog['first']} {prog['last']}"
+    return None
